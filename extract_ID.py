89a06d069fd65de7a9172a9c3fb128e0b163e205
Toby Crocker <tcrocker@mymail.mines.edu>
2020-06-10 22:24:35 -0600

    removed debug code, added comments

--- extract_ID.py
+++ extract_ID.py
@@ -1,7 +1,10 @@
 import sys
 
+#get name. add extension, initialize filestream
 name = sys.argv[1] + ".zip"
-fin = open("foo.txt", "r")
+fin = open("files.txt", "r")
+
+#read lines from file searching for matching name, extract ID & print
 str = fin.readline()
 while not name in str:
     str = fin.readline()
